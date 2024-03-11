dfe97d0dac480ce1b7a06d233ec541203fde9f3b
Martin <m.delsart67@gmail.com>
2024-03-11 08:46:39 +0100

    Heure courante

--- jalon1/exo01/index.php
+++ jalon1/exo01/index.php
@@ -1,4 +1,18 @@
-<?php
-// Display the current time
-echo "Current time: " . date("H:i:s");
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heure courante</title>
+</head>
+<body>
+    <h1>L'heure courante de votre machine est :</h1>
+    <p>
+        <?php
+        date_default_timezone_set('Europe/Paris');
+        $heureCourante = date("H:i:s");
+        echo "Il est actuellement $heureCourante.";
+        ?>
+    </p>
+</body>
+</html>
