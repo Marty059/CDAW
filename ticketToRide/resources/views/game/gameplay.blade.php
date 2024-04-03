2c42577186d8f411b35d92fa4c09dbaf05a760b8
NotArobase <maximegrenet@gmail.com>
2024-04-03 19:14:00 +0200

    commit presque fini placetrain

--- ticketToRide/resources/views/game/gameplay.blade.php
+++ ticketToRide/resources/views/game/gameplay.blade.php
@@ -4,6 +4,10 @@
 use Illuminate\Support\Facades\Redis;
 
 $turn_id=Redis::get('lobby:'.$lobby->id_lobby.':current_turn');
+$available_train_paths=json_decode(Redis::get('lobby:'.$lobby->id_lobby.':available_train_paths'));
+
+
+//$layed_train_paths=json_decode(Redis::get('lobby:'.$lobby->id_lobby.':player:'.auth()->user()->id_user.':layed_train_paths'));
 
 @endphp
 
@@ -12,14 +16,37 @@ $turn_id=Redis::get('lobby:'.$lobby->id_lobby.':current_turn');
 <div class="turn"></div>
 @if($turn_id==auth()->user()->id_user)
     <div class="your-turn">It's your turn!</div>
-<form action="{{ route('game.pickRandomTrainCard', ['lobbyId' => $lobby->id_lobby,'userId' => auth()->user()->id_user]) }}" method="POST">
-    @csrf
-    <button type="submit">Pick a random train card</button>
-</form>
-<form action="{{ route('game.pickDestinationCards', ['lobbyId' => $lobby->id_lobby,'userId' => auth()->user()->id_user]) }}" method="POST">
-    @csrf
-    <button type="submit">Pick 3 destination cards</button>
-</form>
+
+    <form action="{{ route('game.pickRandomTrainCard', ['lobbyId' => $lobby->id_lobby,'userId' => auth()->user()->id_user]) }}" method="POST">
+        @csrf
+        <button type="submit">Pick a random train card</button>
+    </form>
+
+    <form action="{{ route('game.pickDestinationCards', ['lobbyId' => $lobby->id_lobby,'userId' => auth()->user()->id_user]) }}" method="POST">
+        @csrf
+        <button type="submit">Pick 3 destination cards</button>
+    </form>
+    @if($available_train_paths)
+    <div>
+        <h3>Available Paths to Lay Trains</h3>
+        <form action="{{ route('game.placeTrainPath',['lobbyId' => $lobby->id_lobby]) }}" method="POST">
+            @csrf
+            <select name="train_path">
+                @foreach ($available_train_paths as $path)
+                    <option value="{{ $path->id_path }}">
+                    {{ $path->city_1 }} to {{ $path->city_2 }} : {{ $path->length }} 
+                    @if(!($path->colour)) (Any) 
+                    @else() ({{ $path->colour }})
+                    @endif
+                </option>
+                @endforeach
+            </select>
+            <button type="submit">Lay Train</button>
+        </form>
+    </div>
+    @endif
+
+
 @else
     <div class="your-turn">It's not your turn!</div>
 @endif
@@ -33,8 +60,7 @@ $turn_id=Redis::get('lobby:'.$lobby->id_lobby.':current_turn');
 @include('game.map')
 @include('game.cards-to-draw', ['lobbyId' => $lobby->id_lobby])
 @include('game.player-cards', ['lobbyId' => $lobby->id_lobby])
-
-
+@include('game.layed-train-paths', ['lobbyId' => $lobby->id_lobby])
 
 @endsection
 
