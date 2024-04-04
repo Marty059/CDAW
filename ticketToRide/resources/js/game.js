3e02707695614e2fd169dd5f399edcbd37b10416
NotArobase <maximegrenet@gmail.com>
2024-04-05 00:40:56 +0200

    timer 60 secs jeu

--- ticketToRide/resources/js/game.js
+++ ticketToRide/resources/js/game.js
@@ -19,18 +19,18 @@ window.Echo.join(`lobby.${id_lobby}`)
     window.location.reload();
 })
 
-// window.addEventListener('load', function() {
-//     setTimeout(function() {
-//         const lobbyId = window.location.href.split('/').pop();
-//         const yourTurnDiv = document.querySelector('.your-turn');
-//         if (yourTurnDiv) {
-//             fetch(`/game/passTurn/${lobbyId}`, {
-//                 method: 'POST',
-//                 headers: {
-//                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//                 }
-//             });
-//         }
-//     }, 15000); // 1 minute = 60,000 milliseconds
-// });
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        const lobbyId = window.location.href.split('/').pop();
+        const yourTurnDiv = document.querySelector('.your-turn');
+        if (yourTurnDiv) {
+            fetch(`/game/passTurn/${lobbyId}`, {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        }
+    }, 60000); // 1 minute = 60,000 milliseconds
+});
 
